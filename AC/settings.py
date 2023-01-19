e4f5184d270ecd85df8e64fa8309b28a707f8984
Agbai Joshua <realacjoshua@gmail.com>
2023-01-19 23:11:12 +0100

    more edit

--- AC/settings.py
+++ AC/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
-from decouple import config
+#from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     #My apps
     'zicodxapp',
-    
+    'accounts',
     #Default apps
     'django.contrib.admin',
     'django.contrib.auth',
@@ -127,3 +127,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/accounts/dashboard/'
